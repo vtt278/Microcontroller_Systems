384ce7c92313242ef4eaa9c39596f3e7f14d2165
Vincent Tjoa <vintjoa@gmail.com>
2025-09-11 20:57:24 +0700

    Update and rename STM32 Blackpill ESP01_forwarder for Air Quality System.ino to ESP01_forwarder - STM32 Blackpill Air Quality System Adafruit IO upload.ino

--- ESP01_forwarder - STM32 Blackpill Air Quality System Adafruit IO upload.ino
+++ ESP01_forwarder - STM32 Blackpill Air Quality System Adafruit IO upload.ino
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------
-// ESP01_forwarder for STM32F401CCU6 Blackpill ESP01 Air Quality System
+// ESP01_forwarder for STM32F401CCU6 Blackpill ESP01 Air Quality System (Adafruit IO upload)
 // Upload to ESP01 (ESP8266 core).
 // Connect ESP01 TX/RX to STM32 Serial1 (A3/A2) crossed: ESP01 RX <- STM32 TX
 //
